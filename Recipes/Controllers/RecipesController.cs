6e0bd5211c87bc35b81067036b2bd78d8a1cf6f5
Kevin Contreras <kvncont@gmail.com>
2022-10-08 21:12:17 -0600

    Add project files.

---
+++ Recipes/Controllers/RecipesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Recipes.Models;
+using System;
+using System.Net.Mime;
+
+namespace Recipes.Controllers {
+    [Route("api/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ApiController]
+    public class RecipesController : ControllerBase {
+
+        /*private string[] recipes = { "Gallo Pinto", "Tamal Asado", "Arroz con Pollo", "Rice and Beans", "Patacones" };*/
+
+        Recipe[] recipes = {
+                new() { Title = "Gallo Pinto" },
+                new() { Title = "Tamal Asado" },
+                new() { Title = "Arroz con Pollo" },
+                new() { Title = "Rice and Beans" },
+                new() { Title = "Patacones" }
+            };
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult GetRecipes([FromQuery] int count = 5) {
+
+            if (!recipes.Any())
+                return NotFound();
+            return Ok(recipes.Take(count));
+        }
+
+        [HttpPost]
+        public ActionResult CreateRecipe([FromBody] Recipe newRecipe) {
+
+            return Created("", newRecipe);
+        }
+
+        [HttpPut("{index}")]
+        public ActionResult UpdateRecipe(int index) {
+
+            if (index > recipes.Length - 1)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteRecipe(int index) {
+
+            if (index > recipes.Length-1)
+                /*return BadRequest();*/
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Recipes/Models/RecipesModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Recipes.Models {
+    public record Recipe {
+        [JsonPropertyName("recipe_id")]
+        public string? RecipeId { get; init; }
+
+        [Required]
+        public string? Title { get; init; }
+        public string? Description { get; init; }
+        public IEnumerable<string>? Directions { get; init; }
+        public IEnumerable<string>? Tags { get; init; }
+        [Required]
+        public IEnumerable<Ingredient>? Ingredients { get; init; }
+        public DateTime? Updated { get; init; }
+    }
+
+    public record Ingredient {
+        [Required]
+        public string? Name { get; init; }
+        public string? Amount { get; init; }
+        public string? Unit { get; init; }
+    }
+}
